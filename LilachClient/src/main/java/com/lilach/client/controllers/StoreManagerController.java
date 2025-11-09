ea6ab41310998a841d80d4674d5753a6d5475aff
afaf-gharra <afafgharra000@gmail.com>
2025-11-09 22:18:08 +0200

    deligvery time fix

--- LilachClient/src/main/java/com/lilach/client/controllers/StoreManagerController.java
+++ LilachClient/src/main/java/com/lilach/client/controllers/StoreManagerController.java
@@ -1,20 +1,30 @@
 package com.lilach.client.controllers;
 
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.lilach.client.models.OrderDTO;
 import com.lilach.client.models.OrderItemDTO;
 import com.lilach.client.models.ProductDTO;
 import com.lilach.client.models.StoreDTO;
 import com.lilach.client.services.ApiService;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-
-import java.io.IOException;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class StoreManagerController extends BaseController {
     
@@ -111,14 +121,12 @@ public class StoreManagerController extends BaseController {
             });
 
         // set deliveryTypeColumn
-        deliveryTypeColumn.setCellValueFactory(new PropertyValueFactory<>("deliveryType"));
-         deliveryTypeColumn.setCellFactory(column -> new TableCell<OrderDTO, String>() {
-            @Override
-            protected void updateItem(String deliveryType, boolean empty) {
-                super.updateItem(deliveryType, empty);
-                setText(empty || deliveryType == null ? "" : deliveryType);
-            }
-        });        
+        deliveryTypeColumn.setCellValueFactory(cellData -> 
+            javafx.beans.binding.Bindings.createStringBinding(() -> 
+                cellData.getValue().getdeliveryType() != null ?
+                cellData.getValue().getdeliveryType() : ""
+            )
+        );
         // Update status button
         updateStatusButton.setOnAction(e -> updateOrderStatus());
     }
